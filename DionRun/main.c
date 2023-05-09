4398eaa13988f44fc051b181de851b9a954bdfc6
choi <cmk09258@naver.com>
2023-05-09 10:57:45 +0900

    임시저장

--- DionRun/main.c
+++ DionRun/main.c
@@ -32,43 +32,14 @@ int print_how_to_play_screen()
 int main()
 {
 	int game_state = 1;
-	int input;
-	/* 게임 루프
-	 * while문이 계속 반복해서 입력을 받는다.
-	 * 입력받은 값을 game_state에 저장한다.
-	 */
-	while(game_state)
-	{
-		print_title_screen();
 
-		printf("input>");
-		scanf("%d", &input);
+	char name[100];
+	
 
-		if(input == 1)
-		{
-	        	printf("\n");
-	  	}
-		else if(input == 2)
-	{
-		while(1)
-		{
-			print_how_to_play_screen();
-
-			printf("Return to menu? (1.yes 2.no) >");
-			scanf("%d", &input);
-
-		if(input == 1)
-		{
-			break;
-		}
-	    }
-	}
-		else if(input ==3)
-		{
-			game_state =0;
-		}
-	}
-
+	printf("input name>");
+	scanf("%s", &name);
+	printf("his name is %s", name[2]);
 
 	return 0;
+	
 }
